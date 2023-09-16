6eac62e02121467248293f14bb64b014f5968cba
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-16 19:50:18 +0100

    migrations

---
+++ database/migrations/2023_09_16_174430_create_amortizations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('amortizations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('schedule_date');
+            $table->string('state');
+            $table->decimal('amount', 10, 2);
+            $table->foreignId('project_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('amortizations');
+    }
+};
