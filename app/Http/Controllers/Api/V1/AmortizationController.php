b1fb0a15987ea49510573ce6e92e716ccc7a139c
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-17 16:10:07 +0100

    added /check-batch-status and /index - the first checks the status of the jobs - the second fetches all amortizations, with pagination and sort

--- app/Http/Controllers/Api/V1/AmortizationController.php
+++ app/Http/Controllers/Api/V1/AmortizationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Services\AmortizationService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Log;
 
 class AmortizationController extends Controller
@@ -17,9 +19,72 @@ class AmortizationController extends Controller
 
     public function payAllDueAmortizations()
     {
-        Log::info("### Entered the Controller ###");
-        $this->amortizationService->payAllDueAmortizations();
+        try {
+            Log::info("### Entered the Controller ###");
+            $batchId = $this->amortizationService->payAllDueAmortizations();
 
-        return response()->json(['message' => 'Payment process initiated for all due amortizations.']);
+            if ($batchId) {
+                return response()->json([
+                    'message' => 'Payment process initiated for all due amortizations.',
+                    'batchId' => $batchId
+                ], 200);
+            }
+
+            return response()->json([
+                'message' => 'No amortizations to process.',
+            ], 204);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while processing the amortizations.',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function checkBatchStatus(Request $request)
+    {
+        try {
+            $request->validate([
+                'batch_id' => 'required|exists:job_batches,id',
+            ]);
+
+            $batchId = $request->input('batch_id');
+
+            $batch = Bus::findBatch($batchId);
+
+            return response()->json([
+                'id' => $batch->id,
+                'name' => $batch->name,
+                'totalJobs' => $batch->totalJobs,
+                'pendingJobs' => $batch->pendingJobs,
+                'processedJobs' => $batch->processedJobs,
+                'failedJobs' => $batch->failedJobs,
+                'cancelled' => $batch->cancelled(),
+                'finished' => $batch->finished(),
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while checking the batch status.',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function index(Request $request)
+    {
+        try {
+            $perPage = $request->get('per_page', 15); // default 15 items per page
+            $sortBy = $request->get('sort_by', 'id'); // default sort by id
+            $sortOrder = $request->get('sort_order', 'asc'); // default sort order is ascending
+
+            $amortizations = $this->amortizationService->getAllAmortizations($perPage, $sortBy, $sortOrder);
+
+            return response()->json($amortizations);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while fetching the amortizations.',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
