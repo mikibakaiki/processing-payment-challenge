4a19d9e29860ab1f6de83b101b05406136bef6b7
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-17 16:11:34 +0100

    removed chunks and made batchable - may still change this to use both batches and chunks

--- app/Services/AmortizationService.php
+++ app/Services/AmortizationService.php
@@ -4,30 +4,75 @@ namespace App\Services;
 
 use App\Jobs\ProcessAmortizationPaymentJob;
 use App\Models\Amortization;
+use Bus;
 use Carbon\Carbon;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class AmortizationService
 {
-    public function payAllDueAmortizations()
+    public function payAllDueAmortizations(): ?string
     {
-        $currentDate = Carbon::now();
-        Log::info("--- Entered the Service ---");
-        Log::info("Current Date: $currentDate");
+        try {
+            $currentDate = Carbon::now();
+            Log::info("--- Entered the Service ---");
+            Log::info("Current Date: $currentDate");
 
-        $anyAmortizationsProcessed = false;
+            $amortizations = Amortization::where('schedule_date', '<=', $currentDate)
+                ->where('state', '!=', 'paid')
+                ->get();
 
-        Amortization::where('schedule_date', '<=', $currentDate)
-            ->where('state', '!=', 'paid')
-            ->chunk(100, function ($amortizations) use ($currentDate, &$anyAmortizationsProcessed) {
-                $anyAmortizationsProcessed = true;
-                foreach ($amortizations as $amortization) {
-                    Log::info("Processing amortization = $amortization");
-                    ProcessAmortizationPaymentJob::dispatch($amortization, $currentDate);
-                }
-            });
-        if (!$anyAmortizationsProcessed) {
-            Log::info("No amortizations to process.");
+            if ($amortizations->isEmpty()) {
+                Log::info("No amortizations to process.");
+                return null;
+            }
+
+            $jobs = $amortizations->map(function ($amortization) use ($currentDate) {
+                return new ProcessAmortizationPaymentJob($amortization, $currentDate);
+            })->toArray();
+
+            $batch = Bus::batch($jobs)->allowFailures()->dispatch();
+
+            $batchId = $batch->id;
+
+            // Store the batch id or send it to the front end
+            // so that you can check its status later
+
+            Log::info("Batch {$batchId} dispatched");
+            return $batchId;
+        } catch (\Exception $e) {
+            Log::error('An error occurred while dispatching the batch: ' . $e->getMessage());
+            throw $e;
         }
+    }
+
+    public function getAllAmortizations($perPage, $sortBy, $sortOrder)
+    {
+        $amortizations = DB::table('amortizations')
+            ->orderBy($sortBy, $sortOrder)
+            ->paginate($perPage);
+
+        return $amortizations;
+    }
+
+    // To use pagination for the collection
+    public function paginate(Collection $collection, $perPage, $pageName = 'page', $fragment = null)
+    {
+        $currentPage = Paginator::resolveCurrentPage($pageName);
+
+        return new LengthAwarePaginator(
+            $collection->forPage($currentPage, $perPage),
+            $collection->count(),
+            $perPage,
+            $currentPage,
+            [
+                'path' => Paginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+                'fragment' => $fragment,
+            ]
+        );
     }
 }
