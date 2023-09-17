d375b2951c115f9f01cc80f7194b6f3c20953b8b
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-17 16:33:13 +0100

    adding vue, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.{vue,js}',
+    './resources/views/**/*.blade.php',
+  ],
+  theme: {},
+  plugins: [],
+}
+
