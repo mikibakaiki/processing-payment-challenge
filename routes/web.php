be4da0e836f0e617da9f12e16dcfb8d13a44e006
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-20 00:43:25 +0100

    removed vite and tailwind from laravel app - now it has a dedicated folder

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/amortizations', function () {
-    return view('amortizations');
-});
+// Route::get('/amortizations', function () {
+//     return view('amortizations');
+// });
