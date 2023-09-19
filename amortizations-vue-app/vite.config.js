cecca72b6ddddf354de36b24776536ea7fb73664
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-09-19 21:15:38 +0100

    forgotten axios package

--- amortizations-vue-app/vite.config.js
+++ amortizations-vue-app/vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: parseInt(process.env.VITE_APP_PORT, 10) || 3000
+  },
   resolve: {
     alias: {
       '@': '/src',
