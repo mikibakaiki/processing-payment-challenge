a19d4540a47bc0c957d81cf053f74c156c242cc3
João Miguel Campos <joaomiguelcampos6@gmail.com>
2023-09-20 19:50:34 +0000

    trying to connect fe to be

--- amortizations-vue-app/src/axiosInstance.js
+++ amortizations-vue-app/src/axiosInstance.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://laravel.test',
   // you can add more configuration here if needed
 });
 
 instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+instance.defaults.headers.common['Accept'] = 'application/json';
 
 
 export default instance;
